f99b809276383f644c44864fa4aff7dbc10d0499
paolo.veronelli <paolo.veronelli@gmail.com>
2019-09-07 14:47:56 +0000

    add TMFrame

---
+++ src/Persistence/TMFrame.hs
@@ -0,0 +1,41 @@
+-- |
+-- Module      :  Persistence.TMFrame
+-- Copyright   :  Paolo Veronelli, Matthias Putz
+-- License     :  BSD3
+--
+-- Maintainer  :  paolo@global.de
+-- Stability   :  experimental
+-- Portability :  unknown
+--
+-- Support RIO external logging
+--
+
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Persistence.TMFrame where
+
+import           Control.Monad.Reader
+import           Data.Text
+import           Data.Time
+import           RIO
+import Data.Conduit
+
+data TMFrame  = TMFrame 
+    { frameTime    :: Int64 -- ^ earth arrival time 
+    , frameContent   :: ByteString -- ^ raw data received
+    , frameSCID :: Word16 -- ^ statellite id
+    , frameVCID :: Word16 -- ^ 
+    , frameMCFC :: Word8 -- ^ 
+    , frameVCFC :: Word8 -- ^ 
+    }
+  deriving (Generic)
+
+type TMFrameStore = [TMFrame] -> IO ()
+
+data TMQuery 
+    = TMQueryAll 
+
+type TMFrameRetrieve = TMQuery -> ConduitT () TMFrame IO ()
+
+
